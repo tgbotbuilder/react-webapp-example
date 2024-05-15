779c33aca57e044535a3bbe8d79d7643c933d803
Reinier Hernández <sasuke.reinier@gmail.com>
2024-05-15 11:35:16 -0300

    perf: added telegram script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script src="https://telegram.org/js/telegram-web-app.js" />
       <body className={inter.className}>{children}</body>
     </html>
   );
