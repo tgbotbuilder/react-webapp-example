f49848211b73edec84c8b90d435ec3560c65920c
Reinier Hernández <sasuke.reinier@gmail.com>
2024-05-15 11:58:33 -0300

    feat: added Telegram types and show the object

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 'use client'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+    console.log("Hello, world!");
+    console.log(window.Telegram?.WebApp)
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-10 lg:p-24">
+      <h1 className="text-3xl font-bold text-center">Telegram Bot Builder webapp</h1>
+      <pre>
+        {JSON.stringify(window.Telegram?.WebApp, null, 2)}
+      </pre>
     </main>
   );
 }
