0d87498641a10389f8ffe1872daa50dcabadc0a5
Reinier Hernández <sasuke.reinier@gmail.com>
2024-05-15 14:26:22 -0300

    feat: added loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import Loader from "@/components/Loader";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isMounted, setIsMounted] = useState(false);
+
   useEffect(() => {
     setIsMounted(true);
     console.log(window.Telegram?.WebApp)
@@ -14,9 +16,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-10 lg:p-24">
       <h1 className="text-3xl font-bold text-center">Telegram Bot Builder webapp</h1>
-      <pre>
-        {JSON.stringify(window?.Telegram?.WebApp, null, 2)}
-      </pre>
+      {
+        isMounted
+          ? (
+            <pre>
+              {JSON.stringify(window?.Telegram?.WebApp, null, 2)}
+            </pre>
+          )
+          : <Loader />
+      }
     </main>
   );
 }
